024456144a0151fdd727a6df51998dbbce09519a
alvinjaames1 <alvinjaames@gmail.com>
2026-04-05 14:20:33 -0700

    Add files via upload

---
+++ MyNewDishApp/src/test/java/com/NDA/MyNewDishApp/MyNewDishAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NDA.MyNewDishApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewDishAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
